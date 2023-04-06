cecb843d515b62cddd06d6b58660011b641690ab
Nik Tomazic <nick.tomazic@gmail.com>
2023-04-06 12:50:13 +0200

    OS-9, OS-10: installed djangorestframework, swapped SQLite for PostgreSQL, updated README.md

--- README.md
+++ README.md
@@ -11,6 +11,52 @@ You'll notice that there are two requirements files:
 
 Install and freeze to *requirements.lock.txt* and update *requirements.txt* accordingly.
 
+## Development setup
+
+1. Create a new virtual environment.
+2. Install the packages using `pip install -r requirements.txt`.
+3. Create a new `.env` file and put the following inside:
+    ```
+   # Django secret key used for hashing (do not share!)
+   # You can generate it by using the following service: https://djecrety.ir/
+   SECRET_KEY=<generated_django_secret_key>
+   
+   # Can be generated the same way as Django's SECRET_KEY
+   JWT_SECRET_KEY=<generated_jwt_secret_key>
+   
+   DEBUG=true
+   ALLOWED_HOSTS=localhost 127.0.0.1 [::1]
+   CORS_ALLOWED_ORIGINS=http://localhost:3000 http://127.0.0.1:3000
+   TIMEZONE=Europe/Berlin
+   
+   DATABASE_ENGINE=django.db.backends.postgresql_psycopg2
+   DATABASE_NAME=<db_name>
+   DATABASE_USER=<db_user>
+   DATABASE_PASSWORD=<db_password>
+   DATABASE_HOST=<db_host>
+   DATABASE_PORT=<db_port>
+   
+   API_NAME=os-backend
+   API_DESCRIPTION=there are no accidents
+   API_URL=http://localhost:8000/api/
+   API_VERSION=1.0
+   API_AUTHORS=Nik Tomazic (duplxey)
+   
+   STATIC_ROOT=/var/www/static/
+   MEDIA_ROOT=/var/www/media/
+    ```
+4. Get [PostgreSQL](https://www.postgresql.org/) up and running (either with Docker or by [installing it locally](https://www.postgresql.org/download/)).
+    ```sh
+    $ docker run --name <name>-postgres -p 5432:5432 \
+        -e POSTGRES_USER=<db_user> -e POSTGRES_PASSWORD=<db_password> \
+        -e POSTGRES_DB=<db_name> -d postgres
+    ```
+5. Make migrations `python manage.py makemigrations`.
+6. Migrate the database using `python manage.py migrate`.
+7. Run the server using `python manage.py runserver`.
+
+> If you're running Windows, you'll also have to install `psycopg2` using pip.
+
 ## Testing
 
 Before a new release, you should always check if all the tests execute successfully. 
--- core/settings.py
+++ core/settings.py
@@ -9,24 +9,26 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!9^ewcz^(=6(w7q07ea_4d*gdd5yj%w*s6*)cf0&afekiw_k8j"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False).lower() in ["true", "t", "1"]
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
@@ -37,9 +39,17 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
+    # 3rd party
+    "corsheaders",
+    "rest_framework",
+    # custom
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -75,8 +85,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.environ.get("DATABASE_ENGINE"),
+        "NAME": os.environ.get("DATABASE_NAME"),
+        "USER": os.environ.get("DATABASE_USER"),
+        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
+        "HOST": os.environ.get("DATABASE_HOST"),
+        "PORT": os.environ.get("DATABASE_PORT"),
     }
 }
 
@@ -100,12 +114,33 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# API settings
+# these settings are displayed when visiting the index page
+
+API_SETTINGS = {
+    "NAME": os.environ.get("API_NAME"),
+    "DESCRIPTION": os.environ.get("API_DESCRIPTION"),
+    "URL": os.environ.get("API_URL"),
+    "VERSION": os.environ.get("API_VERSION"),
+    "AUTHORS": os.environ.get("API_AUTHORS").split(","),
+}
+
+
+# django-cors-headers
+# https://pypi.org/project/django-cors-headers/
+
+if DEBUG:
+    CORS_ALLOW_ALL_ORIGINS = True
+else:
+    CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS").split(" ")
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = os.environ.get("TIMEZONE")
 
 USE_I18N = True
 
@@ -113,9 +148,14 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.environ.get("STATIC_ROOT")
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.environ.get("MEDIA_ROOT")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
