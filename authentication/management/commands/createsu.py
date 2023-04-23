69ef72a94f208bb28c1d6db93a6b6b055a160311
Nik Tomazic <nick.tomazic@gmail.com>
2023-04-23 16:02:33 +0200

    createsu update

--- authentication/management/commands/createsu.py
+++ authentication/management/commands/createsu.py
@@ -8,8 +8,17 @@ class Command(BaseCommand):
     help = "Creates the super user if it doesn't exist."
 
     def handle(self, *args, **options):
-        su_username = os.environ.get("SUPERUSER_USERNAME")
-        su_password = os.environ.get("SUPERUSER_PASSWORD")
+        su_username = os.environ.get("SUPERUSER_USERNAME", None)
+        su_password = os.environ.get("SUPERUSER_PASSWORD", None)
+
+        if su_username is None or su_password is None:
+            self.stdout.write(
+                self.style.WARNING(
+                    "SUPERUSER_USERNAME and SUPERUSER_PASSWORD environmental variables have not been set. "
+                    "Aborting the superuser creation process."
+                )
+            )
+            return
 
         if User.objects.filter(username=su_username).exists():
             self.stdout.write(self.style.WARNING(f"A superuser with '{su_username}' username already exists."))
