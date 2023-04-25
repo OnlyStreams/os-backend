252bf156dbfda28c68e201ff34896fcb6a8dcaa5
Nik Tomazic <nick.tomazic@gmail.com>
2023-04-25 13:45:51 +0200

    OS-XX: nov

--- core/settings.py
+++ core/settings.py
@@ -20,6 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
 
 
+A = "b"
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
