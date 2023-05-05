e4043bbc8c9645b7baadde0c7d91bc8e52b294c1
Nik Tomazic <nick.tomazic@gmail.com>
2023-05-05 18:03:36 +0200

    OS-XX: OS-90: stream endpoints

--- stream/models.py
+++ stream/models.py
@@ -15,6 +15,7 @@ def generate_stream_key():
 class StreamProfile(models.Model):
     user = models.OneToOneField(to=User, on_delete=models.CASCADE)
     stream_key = models.CharField(max_length=32, default=generate_stream_key)
+    url = models.CharField(max_length=256, blank=True, null=True)
     title = models.CharField(max_length=128, default="Just another stream")
     description = models.TextField(max_length=512, default="", blank=True)
     is_streaming = models.BooleanField(default=False)
